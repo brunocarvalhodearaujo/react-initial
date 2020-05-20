e85e649835da33468f5889af9e5fe6d4c97dcc4b
Bruno <brunocarvalhodearaujo@gmail.com>
2020-05-20 00:37:45 -0300

    update documentation

--- test/Initial.spec.js
+++ test/Initial.spec.js
@@ -6,7 +6,6 @@
  * the root directory of this source tree.
  */
 
-
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Initial from '../src/Initial'
@@ -14,10 +13,15 @@ import Initial from '../src/Initial'
 // https://medium.freecodecamp.org/the-right-way-to-test-react-components-548a4736ab22
 test('check rendered component and create snapshot', () => {
   const component = renderer.create(
-    <Initial name='Varshneya Rao' charCount={3} width={150} useWords={true} />
+    <Initial
+      name='Varshneya Rao'
+      charCount={3}
+      width={150}
+      useWords
+    />
   )
 
-  let tree = component.toJSON()
+  const tree = component.toJSON()
 
   expect(tree.type).toEqual('img')
   expect(tree.props.alt).toEqual('')
